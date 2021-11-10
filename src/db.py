9f0d92effe41cb646223ca01790a68ae57de5565
tamdi <tamyrlan.daniyaruly@gmail.com>
2021-11-10 18:46:35 +0600

    Add files

--- src/db.py
+++ src/db.py
@@ -28,14 +28,18 @@ def sqlalchemy_saver(news_list):
         conn = engine.connect()
         res = conn.execute(result)
 
-def sqlalchemy_getData():
+
+def sqlalchemy_getAllData():
     engine = create_engine('postgresql://postgres:1@localhost:5432/postgres', echo=True)
     meta = MetaData()
     meta.create_all(engine)
     result = engine.execute("SELECT * FROM new")
-    for r in result:
-        print(r)
-def sqlalchemy_deleteData(news_list):
+    # for r in result:
+    #     print(r)
+
+
+def sqlalchemy_getData_by_date(news_list):
+    date_input = input()
     engine = create_engine('postgresql://postgres:1@localhost:5432/postgres', echo=True)
     meta = MetaData()
     meta.create_all(engine)
@@ -51,6 +55,48 @@ def sqlalchemy_deleteData(news_list):
     meta.create_all(engine)
     news_list = json.loads(news_list)
     for new in news_list:
-        delete = news_model.delete().where(news_model.c.date == "2021-10-11")
+        select = news_model.select().where(news_model.c.date == date_input)
+
+        conn = engine.connect()
+        res = conn.execute(select)
+
+
+def sqlalchemy_deleteData_by_date(news_list):
+    date_input = input()
+    engine = create_engine('postgresql://postgres:1@localhost:5432/postgres', echo=True)
+    meta = MetaData()
+    meta.create_all(engine)
+    news_model = Table(
+        'new', meta,
+        Column('id', Integer, primary_key=True),
+        Column('date', Date),
+        Column('title', String),
+        Column('text', String),
+        Column('url', String),
+        Column('img_url', String)
+    )
+    meta.create_all(engine)
+    news_list = json.loads(news_list)
+    for new in news_list:
+        delete = news_model.delete().where(news_model.c.date == date_input)
         conn = engine.connect()
         res = conn.execute(delete)
+
+
+def sqlalchemy_deleteAllData():
+    engine = create_engine('postgresql://postgres:1@localhost:5432/postgres', echo=True)
+    meta = MetaData()
+    meta.create_all(engine)
+    news_model = Table(
+        'new', meta,
+        Column('id', Integer, primary_key=True),
+        Column('date', Date),
+        Column('title', String),
+        Column('text', String),
+        Column('url', String),
+        Column('img_url', String)
+    )
+    meta.create_all(engine)
+    delete = news_model.delete()
+    conn = engine.connect()
+    res = conn.execute(delete)
